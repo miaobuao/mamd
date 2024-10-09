3b03b7f6fd2861421b08ee09e18c241a7ca6775a
miaobuao <miaobuao@outlook.com>
2024-10-09 14:43:53 +0800

    auto locale

--- apps/web/app/app.vue
+++ apps/web/app/app.vue
@@ -1,6 +1,50 @@
 <script setup lang="ts">
 import '@/assets/style/global.css'
+import {
+	useLocalStorage,
+	useNavigatorLanguage,
+	usePreferredLanguages,
+} from '@vueuse/core'
 import { Toaster } from '@/components/ui/sonner'
+
+const { setLocale: _setLocale } = useI18n()
+
+function setLocale(locale: string) {
+	if (/[a-z]+-[a-z]+/i.test(locale)) {
+		_setLocale(locale.split('-')[0]!.toLowerCase())
+	}
+	else if (typeof locale === 'string' && locale.length === 2) {
+		_setLocale(locale.toLocaleLowerCase())
+	}
+	else {
+		_setLocale('en')
+	}
+}
+
+onMounted(() => {
+	const preferredLanguage = usePreferredLanguages()
+	const navigatorLanguage = useNavigatorLanguage()
+	const storageLocale = useLocalStorage<string | undefined>('locale', undefined)
+	watch(
+		[ preferredLanguage, navigatorLanguage.language, storageLocale ],
+		(value) => {
+			const [ preferredLanguage, navigatorLanguage, storageLocale ] = value
+			if (storageLocale) {
+				setLocale(storageLocale)
+			}
+			else if (preferredLanguage.length) {
+				const language = preferredLanguage[0]
+				if (language) {
+					setLocale(language)
+				}
+			}
+			else if (navigatorLanguage) {
+				setLocale(navigatorLanguage)
+			}
+		},
+		{ immediate: true },
+	)
+})
 </script>
 
 <template>
