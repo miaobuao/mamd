d8660793460f9bf48b3687a28acd0371c172c502
miaobuao <miaobuao@outlook.com>
2024-10-10 18:46:35 +0000

    update prisma schema

--- packages/prisma-client-js/prisma/migrations/20241010184127_init/migration.sql
+++ packages/prisma-client-js/prisma/migrations/20241010184127_init/migration.sql
@@ -27,13 +27,13 @@ CREATE TABLE "library" (
     "root" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "modified_at" TIMESTAMP(3) NOT NULL,
-    "linked_dir_id" TEXT NOT NULL,
+    "linked_folder_id" TEXT NOT NULL,
 
     CONSTRAINT "library_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "directory" (
+CREATE TABLE "folder" (
     "id" TEXT NOT NULL,
     "library_id" TEXT NOT NULL,
     "parent_id" TEXT,
@@ -41,7 +41,7 @@ CREATE TABLE "directory" (
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "modified_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "directory_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "folder_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -55,6 +55,18 @@ CREATE TABLE "file" (
     "mtime" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "file_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FileComment" (
+    "id" SERIAL NOT NULL,
+    "fileId" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "modified_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "FileComment_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -86,10 +98,10 @@ CREATE INDEX "user_username_idx" ON "user"("username");
 CREATE INDEX "visible_library_library_id_user_id_idx" ON "visible_library"("library_id", "user_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "library_linked_dir_id_key" ON "library"("linked_dir_id");
+CREATE UNIQUE INDEX "library_linked_folder_id_key" ON "library"("linked_folder_id");
 
 -- CreateIndex
-CREATE INDEX "directory_library_id_parent_id_idx" ON "directory"("library_id", "parent_id");
+CREATE INDEX "folder_library_id_parent_id_idx" ON "folder"("library_id", "parent_id");
 
 -- CreateIndex
 CREATE INDEX "file_sha256_library_id_parent_id_idx" ON "file"("sha256", "library_id", "parent_id");
@@ -107,19 +119,25 @@ ALTER TABLE "visible_library" ADD CONSTRAINT "visible_library_library_id_fkey" F
 ALTER TABLE "library" ADD CONSTRAINT "library_creator_id_fkey" FOREIGN KEY ("creator_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "library" ADD CONSTRAINT "library_linked_dir_id_fkey" FOREIGN KEY ("linked_dir_id") REFERENCES "directory"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "library" ADD CONSTRAINT "library_linked_folder_id_fkey" FOREIGN KEY ("linked_folder_id") REFERENCES "folder"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "directory" ADD CONSTRAINT "directory_library_id_fkey" FOREIGN KEY ("library_id") REFERENCES "library"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "folder" ADD CONSTRAINT "folder_library_id_fkey" FOREIGN KEY ("library_id") REFERENCES "library"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "directory" ADD CONSTRAINT "directory_parent_id_fkey" FOREIGN KEY ("parent_id") REFERENCES "directory"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "folder" ADD CONSTRAINT "folder_parent_id_fkey" FOREIGN KEY ("parent_id") REFERENCES "folder"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "file" ADD CONSTRAINT "file_library_id_fkey" FOREIGN KEY ("library_id") REFERENCES "library"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "file" ADD CONSTRAINT "file_parent_id_fkey" FOREIGN KEY ("parent_id") REFERENCES "directory"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "file" ADD CONSTRAINT "file_parent_id_fkey" FOREIGN KEY ("parent_id") REFERENCES "folder"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FileComment" ADD CONSTRAINT "FileComment_fileId_fkey" FOREIGN KEY ("fileId") REFERENCES "file"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FileComment" ADD CONSTRAINT "FileComment_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "file_tag_on_file" ADD CONSTRAINT "file_tag_on_file_tag_id_fkey" FOREIGN KEY ("tag_id") REFERENCES "file_tag"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
