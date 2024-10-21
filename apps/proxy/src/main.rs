f3405beda5af6c94a59b310acae7788ca57feafc
miaobuao <miaobuao@outlook.com>
2024-10-21 17:14:41 +0800

    fix: http proxy

--- apps/proxy/src/main.rs
+++ apps/proxy/src/main.rs
@@ -1,29 +1,24 @@
 use async_trait::async_trait;
 use pingora::prelude::*;
-use std::sync::Arc;
 use std::env;
 
-pub struct LB(Arc<LoadBalancer<RoundRobin>>);
+pub struct SimpleProxy;
 
 #[async_trait]
-impl ProxyHttp for LB {
-
+impl ProxyHttp for SimpleProxy {
     type CTX = ();
     fn new_ctx(&self) -> () {
         ()
     }
 
     async fn upstream_peer(&self, session: &mut Session, _ctx: &mut ()) -> Result<Box<HttpPeer>> {
-        let upstream = self.0
-            .select(b"", 256)
-            .unwrap();
-        let sni = if session.req_header().uri.path().starts_with("/oss/") {
-            println!("upstream: {}", session.req_header().uri.path());
-            env::var("PROXY_MINIO_UPSTREAMS").unwrap()
+        let path = session.req_header().uri.path();
+        let sni = if path.starts_with("/oss/") {
+            env::var("PROXY_MINIO_UPSTREAM").unwrap()
         } else {
-            env::var("PROXY_WEB_UPSTREAMS").unwrap()
+            env::var("PROXY_WEB_UPSTREAM").unwrap()
         };
-        let peer = Box::new(HttpPeer::new(upstream, false, sni));
+        let peer = Box::new(HttpPeer::new(sni.clone(), false, sni));
         Ok(peer)
     }
 }
@@ -32,16 +27,13 @@ fn main() {
     let mut my_server = Server::new(None).unwrap();
     my_server.bootstrap();
 
-    let upstreams =
-    LoadBalancer::try_from_iter([
-        env::var("PROXY_WEB_UPSTREAMS").unwrap(),
-        env::var("PROXY_MINIO_UPSTREAMS").unwrap(),
-    ]).unwrap();
+    let mut proxy_service = http_proxy_service(&my_server.configuration, SimpleProxy);
+    let addr = format!("0.0.0.0:{}", env::var("PROXY_PORT").unwrap());
+    proxy_service.add_tcp(addr.as_str());
 
-    let mut lb = http_proxy_service(&my_server.configuration, LB(Arc::new(upstreams)));
-    lb.add_tcp("0.0.0.0:6188");
+    my_server.add_service(proxy_service);
 
-    my_server.add_service(lb);
+    println!("proxy server running on http://{}", addr);
 
     my_server.run_forever();
 }
