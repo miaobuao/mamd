6bb810d29c6b4a97d30793f8f297722e4a9b31fd
kimbleex <kimbleex@outlook.com>
2024-10-25 17:09:27 +0800

    list users

--- apps/web/app/pages/index/user/index.vue
+++ apps/web/app/pages/index/user/index.vue
@@ -1,35 +1,21 @@
 <script setup lang="ts">
-import { Badge } from '@/components/ui/badge'
-
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
-import {
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableHeader,
-	TableRow,
-} from '@/components/ui/table'
-import {
-	Tabs,
-	TabsContent,
-} from '@/components/ui/tabs'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import { Tabs, TabsContent } from '@/components/ui/tabs'
+import { Home, LineChart, ListFilter, MoreHorizontal, Package, Package2, PanelLeft, PlusCircle, ShoppingCart, Users2 } from 'lucide-vue-next'
+import { PrismaClient } from 'prisma-client-js'
 
-import {
-	Home,
-	LineChart,
-	ListFilter,
-	MoreHorizontal,
-	Package,
-	Package2,
-	PanelLeft,
-	PlusCircle,
-	ShoppingCart,
-	Users2,
-} from 'lucide-vue-next'
+const prisma = new PrismaClient()
+
+async function getUser() {
+	const user = await prisma.user.findMany({})
+	return user
+}
+
+const users_in_db = await getUser()
 </script>
 
 <template>
@@ -139,17 +125,10 @@ import {
 											<TableHead class="hidden w-[100px] sm:table-cell">
 												<span class="sr-only">Avatar</span>
 											</TableHead>
+											<TableHead>ID</TableHead>
 											<TableHead>Name</TableHead>
-											<TableHead>Account Level</TableHead>
-											<TableHead class="hidden md:table-cell">
-												Price
-											</TableHead>
-											<TableHead class="hidden md:table-cell">
-												Total Sales
-											</TableHead>
-											<TableHead class="hidden md:table-cell">
-												Created Time
-											</TableHead>
+											<TableHead>isManager</TableHead>
+											<TableHead>Created Time</TableHead>
 											<TableHead>
 												<span class="sr-only">Actions</span>
 											</TableHead>
@@ -157,267 +136,27 @@ import {
 									</TableHeader>
 									<!-- content -->
 									<TableBody>
-										<TableRow>
+										<TableRow v-for="user_member in users_in_db" :key="user_member.id">
 											<TableCell class="hidden sm:table-cell">
 												<img
 													alt="Product image"
 													class="aspect-square rounded-md object-cover"
 													height="64"
-													src=""
+													src="./1.jpg"
 													width="64"
 												>
 											</TableCell>
 											<TableCell class="font-medium">
-												Laser Lemonade Machine
+												{{ user_member.id }}
 											</TableCell>
 											<TableCell>
-												<Badge variant="outline">
-													Draft
-												</Badge>
-											</TableCell>
-											<TableCell class="hidden md:table-cell">
-												$499.99
-											</TableCell>
-											<TableCell class="hidden md:table-cell">
-												25
-											</TableCell>
-											<TableCell class="hidden md:table-cell">
-												2023-07-12 10:42 AM
+												{{ user_member.username }}
 											</TableCell>
 											<TableCell>
-												<DropdownMenu>
-													<DropdownMenuTrigger as-child>
-														<Button
-															aria-haspopup="true"
-															size="icon"
-															variant="ghost"
-														>
-															<MoreHorizontal class="h-4 w-4" />
-															<span class="sr-only">Toggle menu</span>
-														</Button>
-													</DropdownMenuTrigger>
-													<DropdownMenuContent align="end">
-														<DropdownMenuLabel>Actions</DropdownMenuLabel>
-														<DropdownMenuItem>Edit</DropdownMenuItem>
-														<DropdownMenuItem>Delete</DropdownMenuItem>
-													</DropdownMenuContent>
-												</DropdownMenu>
-											</TableCell>
-										</TableRow>
-										<TableRow>
-											<TableCell class="hidden sm:table-cell">
-												<img
-													alt="Product image"
-													class="aspect-square rounded-md object-cover"
-													height="64"
-													src=""
-													width="64"
-												>
-											</TableCell>
-											<TableCell class="font-medium">
-												Hypernova Headphones
+												{{ user_member.isAdmin }}
 											</TableCell>
 											<TableCell>
-												<Badge variant="outline">
-													Active
-												</Badge>
-											</TableCell>
-											<TableCell class="hidden md:table-cell">
-												$129.99
-											</TableCell>
-											<TableCell class="hidden md:table-cell">
-												100
-											</TableCell>
-											<TableCell class="hidden md:table-cell">
-												2023-10-18 03:21 PM
-											</TableCell>
-											<TableCell>
-												<DropdownMenu>
-													<DropdownMenuTrigger as-child>
-														<Button
-															aria-haspopup="true"
-															size="icon"
-															variant="ghost"
-														>
-															<MoreHorizontal class="h-4 w-4" />
-															<span class="sr-only">Toggle menu</span>
-														</Button>
-													</DropdownMenuTrigger>
-													<DropdownMenuContent align="end">
-														<DropdownMenuLabel>Actions</DropdownMenuLabel>
-														<DropdownMenuItem>Edit</DropdownMenuItem>
-														<DropdownMenuItem>Delete</DropdownMenuItem>
-													</DropdownMenuContent>
-												</DropdownMenu>
-											</TableCell>
-										</TableRow>
-										<TableRow>
-											<TableCell class="hidden sm:table-cell">
-												<img
-													alt="Product image"
-													class="aspect-square rounded-md object-cover"
-													height="64"
-													src=""
-													width="64"
-												>
-											</TableCell>
-											<TableCell class="font-medium">
-												AeroGlow Desk Lamp
-											</TableCell>
-											<TableCell>
-												<Badge variant="outline">
-													Active
-												</Badge>
-											</TableCell>
-											<TableCell class="hidden md:table-cell">
-												$39.99
-											</TableCell>
-											<TableCell class="hidden md:table-cell">
-												50
-											</TableCell>
-											<TableCell class="hidden md:table-cell">
-												2023-11-29 08:15 AM
-											</TableCell>
-											<TableCell>
-												<DropdownMenu>
-													<DropdownMenuTrigger as-child>
-														<Button
-															aria-haspopup="true"
-															size="icon"
-															variant="ghost"
-														>
-															<MoreHorizontal class="h-4 w-4" />
-															<span class="sr-only">Toggle menu</span>
-														</Button>
-													</DropdownMenuTrigger>
-													<DropdownMenuContent align="end">
-														<DropdownMenuLabel>Actions</DropdownMenuLabel>
-														<DropdownMenuItem>Edit</DropdownMenuItem>
-														<DropdownMenuItem>Delete</DropdownMenuItem>
-													</DropdownMenuContent>
-												</DropdownMenu>
-											</TableCell>
-										</TableRow>
-										<TableRow>
-											<TableCell class="hidden sm:table-cell">
-												<img
-													alt="Product image"
-													class="aspect-square rounded-md object-cover"
-													height="64"
-													src=""
-													width="64"
-												>
-											</TableCell>
-											<TableCell class="font-medium">
-												TechTonic Energy Drink
-											</TableCell>
-											<TableCell>
-												<Badge variant="secondary">
-													Draft
-												</Badge>
-											</TableCell>
-											<TableCell class="hidden md:table-cell">
-												$2.99
-											</TableCell>
-											<TableCell class="hidden md:table-cell">
-												0
-											</TableCell>
-											<TableCell class="hidden md:table-cell">
-												2023-12-25 11:59 PM
-											</TableCell>
-											<TableCell>
-												<DropdownMenu>
-													<DropdownMenuTrigger as-child>
-														<Button
-															aria-haspopup="true"
-															size="icon"
-															variant="ghost"
-														>
-															<MoreHorizontal class="h-4 w-4" />
-															<span class="sr-only">Toggle menu</span>
-														</Button>
-													</DropdownMenuTrigger>
-													<DropdownMenuContent align="end">
-														<DropdownMenuLabel>Actions</DropdownMenuLabel>
-														<DropdownMenuItem>Edit</DropdownMenuItem>
-														<DropdownMenuItem>Delete</DropdownMenuItem>
-													</DropdownMenuContent>
-												</DropdownMenu>
-											</TableCell>
-										</TableRow>
-										<TableRow>
-											<TableCell class="hidden sm:table-cell">
-												<img
-													alt="Product image"
-													class="aspect-square rounded-md object-cover"
-													height="64"
-													src=""
-													width="64"
-												>
-											</TableCell>
-											<TableCell class="font-medium">
-												Gamer Gear Pro Controller
-											</TableCell>
-											<TableCell>
-												<Badge variant="outline">
-													Active
-												</Badge>
-											</TableCell>
-											<TableCell class="hidden md:table-cell">
-												$59.99
-											</TableCell>
-											<TableCell class="hidden md:table-cell">
-												75
-											</TableCell>
-											<TableCell class="hidden md:table-cell">
-												2024-01-01 12:00 AM
-											</TableCell>
-											<TableCell>
-												<DropdownMenu>
-													<DropdownMenuTrigger as-child>
-														<Button
-															aria-haspopup="true"
-															size="icon"
-															variant="ghost"
-														>
-															<MoreHorizontal class="h-4 w-4" />
-															<span class="sr-only">Toggle menu</span>
-														</Button>
-													</DropdownMenuTrigger>
-													<DropdownMenuContent align="end">
-														<DropdownMenuLabel>Actions</DropdownMenuLabel>
-														<DropdownMenuItem>Edit</DropdownMenuItem>
-														<DropdownMenuItem>Delete</DropdownMenuItem>
-													</DropdownMenuContent>
-												</DropdownMenu>
-											</TableCell>
-										</TableRow>
-										<TableRow>
-											<TableCell class="hidden sm:table-cell">
-												<img
-													alt="Product image"
-													class="aspect-square rounded-md object-cover"
-													height="64"
-													src=""
-													width="64"
-												>
-											</TableCell>
-											<TableCell class="font-medium">
-												Luminous VR Headset
-											</TableCell>
-											<TableCell>
-												<Badge variant="outline">
-													Active
-												</Badge>
-											</TableCell>
-											<TableCell class="hidden md:table-cell">
-												$199.99
-											</TableCell>
-											<TableCell class="hidden md:table-cell">
-												30
-											</TableCell>
-											<TableCell class="hidden md:table-cell">
-												2024-02-14 02:14 PM
+												{{ user_member.ctime }}
 											</TableCell>
 											<TableCell>
 												<DropdownMenu>
