a7543c117f31b4ce09777ea3dcb4062d81dc6b0b
miaobuao <miaobuao@outlook.com>
2025-01-31 19:25:25 +0800

    feat: migrate configuration to runtimeConfig and remove deprecated config file

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,3 +1,6 @@
+import process from 'node:process'
+import { isNil } from 'lodash-es'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	future: {
@@ -44,5 +47,19 @@ export default defineNuxtConfig({
 
 	compatibilityDate: '2024-11-13',
 
-	runtimeConfig: {},
+	runtimeConfig: {
+		// JWT
+		JWT_SECRET: process.env.JWT_SECRET!,
+		JWT_ISSUER: process.env.JWT_ISSUER!,
+		OAUTH_JWT_EXPIRES_IN: process.env.OAUTH_JWT_EXPIRES_IN!,
+
+		// MINIO
+		MINIO_ENDPOINT: process.env.MINIO_ENDPOINT!,
+		MINIO_API_PORT: isNil(process.env.MINIO_API_PORT) ? undefined : Number.parseInt(process.env.MINIO_API_PORT!),
+		MINIO_ACCESS_KEY: process.env.MINIO_ACCESS_KEY!,
+		MINIO_SECRET_KEY: process.env.MINIO_SECRET_KEY!,
+		MINIO_USE_SSL: isNil(process.env.MINIO_USE_SSL) ? undefined : process.env.MINIO_USE_SSL.toLowerCase() === 'true',
+
+		DATABASE_URL: process.env.DATABASE_URL!,
+	},
 })
--- apps/web/server/utils/minio.client.ts
+++ apps/web/server/utils/minio.client.ts
@@ -1,11 +1,11 @@
 import { isNil } from 'lodash-es'
 import * as Minio from 'minio'
-import { config } from './config'
 
 let minioClient: Minio.Client | undefined
 
 export function useMinioClient() {
 	if (isNil(minioClient)) {
+		const config = useRuntimeConfig()
 		minioClient = new Minio.Client({
 			endPoint: config.MINIO_ENDPOINT,
 			port: config.MINIO_API_PORT,
