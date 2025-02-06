f4719c810f8ce39dbf35af21e5956c01ce5bf1ed
miaobuao <miaobuao@outlook.com>
2025-02-06 22:47:31 +0800

    feat: avatar components

---
+++ apps/web/app/components/user/Avatar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{
+	alt?: string
+	id: string
+}>()
+
+// TODO: username or props.alt
+const altText = computed(() => props.alt)
+</script>
+
+<template>
+	<Avatar v-bind="$attrs">
+		<!-- TODO: user avatar -->
+		<AvatarFallback>
+			{{ altText }}
+		</AvatarFallback>
+	</Avatar>
+</template>
