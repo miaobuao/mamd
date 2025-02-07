6a071ea53a13a6e9275fe3bb7f7b3f0a9437beff
miaobuao <miaobuao@outlook.com>
2025-02-08 04:19:25 +0800

    feat: create new user

---
+++ apps/web/server/api/v1/admin/user/index.post.ts
@@ -0,0 +1,42 @@
+/**
+ * call by admin to create new account
+ */
+import type { TypeOf } from 'zod'
+import { UserTable } from 'drizzle-client'
+import { eq } from 'drizzle-orm'
+import { AssertUserIsAdmin } from '../middleware/assert-user-is-admin'
+
+export default defineEventHandler<
+	{
+		body: TypeOf<typeof CreateUserInputValidator>
+	},
+	Promise<{
+		id: string
+	}>
+>({
+	// TODO: more granular permission control
+	onRequest: [ AssertUserIsAdmin ],
+	handler: async (event) => {
+		const input = await readBody(event)
+		const { context: { db } } = event
+		const hasSameUsername = await db.query.UserTable.findFirst({
+			where: eq(UserTable.username, input.username),
+		})
+		if (hasSameUsername) {
+			throw new ForbiddenErrorWithI18n(i18n.error.usernameHasBeenRegistered)
+		}
+		const hashPassword = await bcryptEncrypt(input.password)
+		return await db
+			.insert(UserTable)
+			.values({
+				username: input.username,
+				password: hashPassword,
+				isAdmin: input.isAdmin,
+			})
+			.returning({ id: UserTable.id })
+			.then((d) => d.at(0)!)
+			.catch(() => {
+				throw new ForbiddenErrorWithI18n(i18n.error.registerFailed)
+			})
+	},
+})
