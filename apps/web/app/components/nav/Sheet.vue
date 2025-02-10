1886b5ea4ee59dbdec6859689190526cdf1c020d
miaobuao <miaobuao@outlook.com>
2025-02-11 03:35:59 +0800

    feat: panel icon support different direction

--- apps/web/app/components/nav/Sheet.vue
+++ apps/web/app/components/nav/Sheet.vue
@@ -1,16 +1,28 @@
 <script setup lang="ts">
-import { Home, LineChart, PanelLeft } from 'lucide-vue-next'
+import {
+	Home,
+	LineChart,
+	PanelBottom,
+	PanelLeft,
+	PanelRight,
+	PanelTop,
+} from 'lucide-vue-next'
 
-defineProps<{
+withDefaults(defineProps<{
 	side?: 'left' | 'right' | 'top' | 'bottom' | null
-}>()
+}>(), {
+	side: 'left',
+})
 </script>
 
 <template>
 	<Sheet>
 		<SheetTrigger as-child>
 			<Button size="icon" variant="outline" v-bind="$attrs">
-				<PanelLeft class="h-5 w-5" />
+				<PanelLeft v-if="side === 'left'" class="h-5 w-5" />
+				<PanelRight v-if="side === 'right'" class="h-5 w-5" />
+				<PanelTop v-if="side === 'top'" class="h-5 w-5" />
+				<PanelBottom v-if="side === 'bottom'" class="h-5 w-5" />
 			</Button>
 		</SheetTrigger>
 		<SheetContent :side="side" class="sm:max-w-xs">
